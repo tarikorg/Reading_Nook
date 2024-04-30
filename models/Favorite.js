764bf2f973e407b1305c65df3647e1b91f5e9c44
tarikorg <myku1771@gmail.com>
2024-04-30 14:30:13 -0400

    commit backend Trevor/kendell/tarik, frontend thomas/mirsad day1

---
+++ models/Favorite.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize')
+const client = require('../db/Connection')
+const bcrypt = require('bcrypt')
+
+class Favorite extends Model {
+
+}
+//id
+//books-id
+//user-id
+
+Favorite.init({
+    id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title:{
+        type: DataTypes.STRING,
+        allowNull: false,
+
+      }
+      },
+      {
+       sequelize: client,
+       timestamps: false,
+       modelName: 'favorite'
+      }
+)
+
+module.exports = Favorite;
