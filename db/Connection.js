18c38c5f364d9c5b9210755aa2567269b701bc59
tarikorg <myku1771@gmail.com>
2024-05-02 14:47:45 -0400

    commit deployment changes

--- db/Connection.js
+++ db/Connection.js
@@ -1,7 +1,7 @@
 
 const Sequelize = require('sequelize')
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'postgres',
