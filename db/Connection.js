275dc343bc316dedc9338d7cd18e2d9753e24dc1
tarikorg <myku1771@gmail.com>
2024-05-02 14:53:26 -0400

    commit fixed connection

--- db/Connection.js
+++ db/Connection.js
@@ -1,7 +1,15 @@
 
 const Sequelize = require('sequelize')
 const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL)
+  ? new Sequelize(process.env.DATABASE_URL,
+    {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    })
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'postgres',
